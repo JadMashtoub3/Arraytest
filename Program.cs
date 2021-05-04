85bccf7e9d5466124a482029a76497358fdbe497
JadMashtoub3 <80442672+JadMashtoub3@users.noreply.github.com>
2021-05-04 12:12:05 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Example
+{
+    public static void Main()
+    {
+
+        int[] array = { 1, 2, 3, 4, 5 };
+        
+        int sum = 0;
+        Array.ForEach(array, i => sum += i);
+        Console.WriteLine(sum);
+        
+            
+        
+        
+           
+        }
+    }
+
